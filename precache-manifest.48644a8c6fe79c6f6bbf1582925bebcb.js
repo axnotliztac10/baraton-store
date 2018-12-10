7cd1f523fe932ca36a7f17c9bd1489802f6239e8
s7990027 <lflopeze@scotiabank.com.mx>
2018-12-10 11:37:39 -0600

    Updates

---
+++ precache-manifest.48644a8c6fe79c6f6bbf1582925bebcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db9b59a8173447468fb1",
+    "url": "/baraton-store/static/js/runtime~main.db9b59a8.js"
+  },
+  {
+    "revision": "9b4adc84a7ef92d0bb73",
+    "url": "/baraton-store/static/js/main.9b4adc84.chunk.js"
+  },
+  {
+    "revision": "d1de32350012335bd496",
+    "url": "/baraton-store/static/js/1.d1de3235.chunk.js"
+  },
+  {
+    "revision": "9b4adc84a7ef92d0bb73",
+    "url": "/baraton-store/static/css/main.0fa3720b.chunk.css"
+  },
+  {
+    "revision": "609ba5a4e8759df17fae0ede93e1847a",
+    "url": "/baraton-store/index.html"
+  }
+];
